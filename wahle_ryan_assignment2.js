28d98a86bad8200a4019c2fbc7626faa34f2f707
Ryan Wahle <ryanwahle@fullsail.edu>
2013-01-16 19:40:17 -0700

    Added the required variables and the procedure function

--- wahle_ryan_assignment2.js
+++ wahle_ryan_assignment2.js
@@ -1,4 +1,22 @@
-alert("JavaScript works!");
+// Ryan Wahle
+// SDI 1301
+// January 16, 2013
+// Project 2
+// Sneaking into Work Late
 
-blah stuff
-why isn't this being commited?
+// Setup some variables
+
+var theLateGuysName = "James";
+var numberOfMinutesLate = 20;
+var coworkerNames = ["Jeff", "Sarah", "Jimbob", "Mark"];
+
+// Procedure
+var securityDesk = function(name) {
+	if (name == "James") {
+		console.log("Hey James! Good morning! Did you have a good weekend?");
+	} else {
+		console.log("Good Morning! Have a great day today!");
+	}
+}
+
+securityDesk(theLateGuysName);
