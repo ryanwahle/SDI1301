0082b73050d3d800b2db266d2be721a7da6a3b08
Ryan Wahle <ryanwahle@fullsail.edu>
2013-01-16 21:05:03 -0700

    Added the array function.

--- wahle_ryan_assignment2.js
+++ wahle_ryan_assignment2.js
@@ -7,7 +7,7 @@
 // Setup some variables
 
 var theLateGuysName = "James";
-var numberOfMinutesLate = 20;
+var numberOfMinutesLate = 19;
 var coworkerNames = ["Jeff", "Sarah", "Jimbob", "Mark"];
 
 // Procedure
@@ -40,14 +40,38 @@ var goodMorningYells = function(numberOfCoworkers) {
 	return numberOfGoodmornings;
 }
 
+// String Function
+var loudSpeaker = function(name1, name2) {
+	var newString = "";
+
+	newString = "Attention! Attention! " + name1 + "!! Please come to my office now! This is " + name2 + "!!";
+
+	return newString;
+}
+
+// Array Function
+var ratCoworkers = function(arrayCoworkers, numberLateness) {
+	var numberOfCoworkers = arrayCoworkers.length;
+	var madCoworkers = [];
+	
+	for (var currentWorker = 0; currentWorker < numberOfCoworkers; currentWorker++) {
+
+		if (numberLateness % currentWorker) {
+			madCoworkers.push(arrayCoworkers[currentWorker]);
+		}
+	}
+
+	return madCoworkers;
+}  
+
 // Walks into building
 console.log(theLateGuysName + " walks into the building and shows his badge to the guard.");
 securityDesk(theLateGuysName);
 
 // See's first coworker
 console.log(theLateGuysName + " walks past security and runs into " + coworkerNames[0] + ".");
-var myReturn = isCoworkerCool(theLateGuysName, coworkerNames[0]);
 
+var myReturn = isCoworkerCool(theLateGuysName, coworkerNames[0]);
 if (myReturn == true) {
 	console.log(coworkerNames[0] + " gives you the nod and ignores the fact you are late, since you two are friends.");
 } else {
@@ -57,3 +81,14 @@ if (myReturn == true) {
 // Good morning!
 var numberOfGoodMornings = goodMorningYells(coworkerNames.length);
 console.log("Oh great! Just what you need! " + numberOfGoodMornings + " people telling you Good Morning as your late!!");
+
+// Sits at his desk
+console.log("As you sit down to read your email you hear something scary on the loud speaker!");
+console.log("Loudspeaker: " + loudSpeaker(theLateGuysName, "your Boss"));
+
+// Go and see the boss man!
+var coworkersWhoHateYou = ratCoworkers(coworkerNames, numberOfMinutesLate);
+console.log("Boss Man: Listen here " + theLateGuysName + "!! Your always late and today I got " + coworkersWhoHateYou.length + " more complaints about you!");
+console.log("Boss Man: YOUR FIRED!!");
+
+console.log("-- The End --");
