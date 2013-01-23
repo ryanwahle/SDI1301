2afdf6fa02376fffa57b9e95cf4c670c61c23b80
Ryan Wahle <ryanwahle@fullsail.edu>
2013-01-23 14:56:26 -0700

    Added the procedure for setting up coworkerObjects

--- wahle_ryan_assignment3.js
+++ wahle_ryan_assignment3.js
@@ -7,6 +7,24 @@
 // Global Variables
 var name = "Ryan";
 var numberMinutesLate = 30;
-var coworkerInformation = json;
+var coworkerInformation = [];
 
+// This procedure takes the json data and sets up the coworker Object
+var setupCoworkersObject = function (json) {
+	for (var i = 0; i < json.workers.length; i++) {
+		var worker = json.workers[i];
 
+		var tempObject = {
+			"name": worker.name,
+			"ranking": worker.ranking,
+			"workdays": worker.workdays	
+		};
+
+		coworkerInformation.push(tempObject);	
+
+	}	
+};
+
+setupCoworkersObject(json);
+
+console.log(coworkerInformation);
